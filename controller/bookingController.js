ff0b83a151381ec96807d37f7ba5c70fdcef23d0
EdduOlv <olivareseduardo.saez@gmail.com>
2024-08-23 20:55:53 -0400

    validaciones listas

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -17,13 +17,12 @@ exports.createBooking = async (req, res) => {
     paymentStatus,
   } = req.body;
 
-  const formatedEntryDate = moment(entryDate, "DD-MM-YYYY").format("YYYY-MM-DD");
+  const formatedEntryDate = moment(entryDate,"DD-MM-YYYY").format("YYYY-MM-DD");
   const formatedEgressDate = moment(egressDate, "DD-MM-YYYY").format("YYYY-MM-DD");
   const correctSchedule = moment(formatedEgressDate).isBefore(formatedEntryDate)
 
   const emailFound = Bookings.find(booking => booking.guestEmail === guestEmail);
   const phoneFound = Bookings.find(booking => booking.guestPhone === guestPhone);
-
 
   if (correctSchedule) {
     return res.status(400).json({ msg: 'Ingrese un margen de fechas de ingraso y egreso validas' });
@@ -55,8 +54,11 @@ exports.createBooking = async (req, res) => {
 
 exports.getBooking = async (req, res) =>{
     const {hotel, tipo_habitacion, fecha_inicio, fecha_fin, estado, num_huespedes, consulta_fecha} = req.query
+
     if (hotel) {
+
         const bookingFiltered = Bookings.filter(booking => booking.hotel === hotel);
+
         if (bookingFiltered.length === 0 ) {
 
             return res.status(404).json({ msg: 'No se encontraron reservas' });
@@ -84,7 +86,9 @@ exports.getBooking = async (req, res) =>{
             })  
         }
     }else if (estado) {
+
         const bookingFiltered = Bookings.filter(booking => booking.paymentStatus === estado);
+
         if (bookingFiltered.length === 0 ) {
 
             return res.status(404).json({ msg: 'No se encontraron reservas' });
@@ -112,7 +116,9 @@ exports.getBooking = async (req, res) =>{
             })  
         }
     }else if (tipo_habitacion) {
+
         const bookingFiltered = Bookings.filter(booking => booking.roomCategory === tipo_habitacion);
+
         if (bookingFiltered.length === 0 ) {
 
             return res.status(404).json({ msg: 'No se encontraron reservas' });
@@ -135,12 +141,14 @@ exports.getBooking = async (req, res) =>{
                 return res.status(404).json({ msg: 'No se encontraron reservas para mes de la consulta' });
             }
             return res.json({
-                msg: `Las reservas encontradas con el estado: ${estado} segun la fecha indicada son: `,
+                msg: `Las reservas encontradas con el tipo de habitacion ${tipo_habitacion} segun la fecha indicada son: `,
                 data: queryByDate
             })  
         }
     }else if (num_huespedes) {
+
         const bookingFiltered = Bookings.filter(booking => booking.numGuests === num_huespedes);
+
         if (bookingFiltered.length === 0 ) {
 
             return res.status(404).json({ msg: 'No se encontraron reservas' });
@@ -168,20 +176,27 @@ exports.getBooking = async (req, res) =>{
             })  
         }
     }else if (fecha_inicio && fecha_fin) {
-        const bookingFiltered = Bookings.filter(booking => moment(booking.entryDate).isBetween(fecha_inicio, fecha_fin, null, '[]'));
+        
+        const formatedEntryDate = moment(fecha_inicio, "DD-MM-YYYY").format("YYYY-MM-DD");
+        const formatedEgressDate = moment(fecha_inicio, "DD-MM-YYYY").format("YYYY-MM-DD");
+        const bookingFiltered = Bookings.filter(booking => moment(booking.entryDate).isBetween(formatedEntryDate, formatedEgressDate, null, '[]'));
+
         if (bookingFiltered.length === 0 ) {
 
-            return res.status(404).json({ msg: 'No se encontraron reservas' });
+            return res.status(404).json({ msg: 'No se encontraron reservas en el margen de fechas indicado' });
         }
         return res.json({
             msg: "Reservas encontradas segun el rango de fechas de la consulta son: ",
             data: bookingFiltered
         })
     }else if (!hotel && !tipo_habitacion && !estado && !num_huespedes && consulta_fecha) {
-        const bookingFiltered = Bookings.filter(booking => booking.entryDate === consulta_fecha);
+
+        const formattedDate = moment(consulta_fecha, "DD-MM-YYYY").format("YYYY-MM-DD");
+        const bookingFiltered = Bookings.filter(booking => booking.entryDate === formattedDate);
+        
         if (bookingFiltered.length === 0 ) {
 
-            return res.status(404).json({ msg: 'No se encontraron reservas algo mal salio aca' });
+            return res.status(404).json({ msg: 'No se encontraron reservas para la fecha indicada' });
         }
         return res.json({
             msg: `Reservas encontradas para la fecha ${consulta_fecha} son: `,
@@ -197,13 +212,11 @@ exports.getBooking = async (req, res) =>{
 }
 
 exports.getBookingById = async (req, res) =>{
-    const {bookingId} = req.params.id
-    const booking = Bookings.find(booking.id === bookingId)
-
+    const id = req.params.id
+    const booking = Bookings.find(booking => booking.id === id)
     if (!booking) {
-        return res.status(404).json({ msg: 'No se encontraro ninguna reserva' });
+        return res.status(404).json({ msg: 'No se encontro ninguna reserva' });
     }
-
     return res.json({
         msg: "Reserva encontrada con exito",
         data: booking
@@ -211,7 +224,7 @@ exports.getBookingById = async (req, res) =>{
 }
 
 exports.updateBookingById = async (req, res) =>{
-    const {bookingId} = req.params.id
+    const id = req.params.id
     const {
         numGuests,
         roomCategory,
@@ -219,44 +232,68 @@ exports.updateBookingById = async (req, res) =>{
         egressDate,
         guestEmail,
         guestPhone,
-      } = req.body;
+    } = req.body;
 
-    const bookingIndex = Bookings.findIndex(booking => booking.id === bookingId)
-
+    const bookingIndex = Bookings.findIndex(booking => booking.id === id)
+    
     if (bookingIndex === -1) {
-        return res.status(404).json({ msg: 'No se encontraro ninguna reserva' });
+        return res.status(404).json({ msg: 'No se encontro ninguna reserva' });
     }
 
-    const formatedEntryDate = moment(entryDate, "DD-MM-YYYY").format("YYYY-MM-DD");
-    const formatedEgressDate = moment(egressDate, "DD-MM-YYYY").format("YYYY-MM-DD");
-    const correctSchedule = moment(formatedEgressDate).isBefore(formatedEntryDate)
-
+    if (entryDate || egressDate) {
+        if (entryDate && egressDate) {
+            const formatedEntryDate = moment(entryDate, "DD-MM-YYYY").format("YYYY-MM-DD");
+            const formatedEgressDate = moment(egressDate, "DD-MM-YYYY").format("YYYY-MM-DD");
+            const correctSchedule = moment(formatedEgressDate).isBefore(formatedEntryDate)
+            if (correctSchedule) {
+                return res.status(400).json({ msg: 'Ingrese un margen de fechas de entrada y salida validas' });
+            }
+        } else if (entryDate && !egressDate){
+            const formatedEntryDate = moment(entryDate, "DD-MM-YYYY").format("YYYY-MM-DD");
+            const egressDateFound = Bookings.find(booking => booking.id === id);
+            const correctSchedule = moment(egressDateFound.egressDate).isBefore(formatedEntryDate)
+            if (correctSchedule) {
+                return res.status(400).json({ msg: 'Ingrese una fecha de entrada valida' });
+            }
+        }else if (!entryDate && egressDate){
+            const formatedEgressDate = moment(egressDate, "DD-MM-YYYY").format("YYYY-MM-DD");
+            console.log(formatedEgressDate);
+            const entryDateFound = Bookings.find(booking => booking.id === id);
+            const correctSchedule = moment(formatedEgressDate).isBefore(entryDateFound.entryDate)
+            if (correctSchedule) {
+                return res.status(400).json({ msg: 'Ingrese una fecha de salida valida' });
+            }
+        }     
+    }
+ 
     const emailFound = Bookings.find(booking => booking.guestEmail === guestEmail);
     const phoneFound = Bookings.find(booking => booking.guestPhone === guestPhone);
-
-    if (correctSchedule) {
-        return res.status(400).json({ msg: 'Ingrese un margen de fechas de ingraso y egreso validas' });
-    } else if ((roomCategory === "estandar" && numGuests >= 3) || (roomCategory === "doble" && numGuests >= 7)) {
+    
+    if ((roomCategory === "estandar" && numGuests >= 3) || (roomCategory === "doble" && numGuests >= 7)) {
         return res.status(400).json({ msg: `Sobrepasa el numero de huéspedes para una habitacion de categoría ${roomCategory}` });
-    }else if (emailFound) {
-        if (bookingIndex.id != emailFound.id) {
-            return res.status(400).json({ msg: `Este email de contacto ya esta registrado`});
-        } 
-    }else if (phoneFound) {
-        if (bookingIndex.id != phoneFound.id) {
-            return res.status(400).json({ msg: `Este telefono de contacto ya esta registrado`});
-        }
     }
+
+    if (emailFound || phoneFound) {
+        if (id != emailFound.id) {
+            return res.status(400).json({ msg: `Este email de contacto ya esta registrado`});
+            
+        }
+        if (id != phoneFound.id) {
+            return res.status(400).json({ msg: `Este telefono de contacto ya esta registrado`});
+            
+        } 
+    }
+
     Bookings[bookingIndex] = { ...Bookings[bookingIndex], ...req.body}
     return res.json({
         msg: "Reserva actualizada con exito",
-        data: Bookings[bookingIndex]
+        data: Bookings[bookingIndex], 
     })  
 }
 
 exports.deleteBookingById = async (req, res) =>{
-    const {bookId} = req.params.id
-    const bookingIndex = Bookings.findIndex(booking => booking.id === bookId)
+    const id = req.params.id
+    const bookingIndex = Bookings.findIndex(booking => booking.id === id)
 
     if (bookingIndex === -1) {
         return res.status(404).json({ msg: 'No se encontraro ninguna reserva' });
@@ -266,5 +303,5 @@ exports.deleteBookingById = async (req, res) =>{
 
     return res.json({
         msg: "Reserva eliminada con exito",
-    })
+    });
 }
